c1904e3cbf3c822e5603e27d98a9f4c7f153fd3c
unknown <adil.muhammad93@gmail.com>
2017-11-16 20:08:02 +0500

    todo crud app work

--- todo-crud-app/src/app/app.module.ts
+++ todo-crud-app/src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { ModalComponent } from './modal/modal.component';
+import { UpdateComponent } from './update/update.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModalComponent,
+    UpdateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
